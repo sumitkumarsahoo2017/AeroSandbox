5717c59c4ffc45dbce07fe9d7a7a5c53eafdc604
sumitkumarsahoo2017 <sumitkumarsahoo2017@gmail.com>
2024-02-13 12:38:08 +0530

    commited

---
+++ aerosandbox/atmosphere/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+### MARTIAN ATMOSPHERE
+# ### Define constants
+# gas_constant_universal = 8.31432  # J/(mol*K); universal gas constant
+# molecular_mass_air = 43.2812e-3  # kg/mol; molecular mass of air
+# gas_constant_air = gas_constant_universal / molecular_mass_air  # J/(kg*K); gas constant of air
+# effective_collision_diameter = 0.365e-9  # m, effective collision diameter of an air molecule
+
