7938d6611d7f7aee3e7f812f6e0d8719d239bcac
sumitkumarsahoo2017 <sumitkumarsahoo2017@gmail.com>
2024-02-14 13:02:31 +0530

    changed range of linspace to 0 to 10000

--- aerosandbox/atmosphere/_diff_atmo_functions.py
+++ aerosandbox/atmosphere/_diff_atmo_functions.py
@@ -17,7 +17,7 @@ from aerosandbox.atmosphere._isa_atmo_functions import pressure_isa, temperature
 # )
 
 # altitude_knot_points = np.sort(np.unique(altitude_knot_points))
-altitude_knot_points = np.linspace(0, 20e3, 1000)
+altitude_knot_points = np.linspace(0, 10e3, 1000)
 print('altitude knot points =',altitude_knot_points)
 temperature_knot_points = temperature_isa(altitude_knot_points)
 pressure_knot_points = pressure_isa(altitude_knot_points)
